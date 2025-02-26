a37f9ad4baa93033fe60e60b788196983270edeb
hugpham23 <hung.phamdata@gmail.com>
2025-02-26 14:37:24 +0700

    Initial commit

---
+++ deployment/native/styles.js
@@ -0,0 +1,20 @@
+import * as style0 from "E:/quizapp-mx-course-master/themesource/administration/native/main";
+import * as style1 from "E:/quizapp-mx-course-master/themesource/communitycommons/native/main";
+import * as style2 from "E:/quizapp-mx-course-master/themesource/documentgeneration/native/main";
+import * as style3 from "E:/quizapp-mx-course-master/themesource/nanoflowcommons/native/main";
+import * as style4 from "E:/quizapp-mx-course-master/themesource/randomobject/native/main";
+import * as style5 from "E:/quizapp-mx-course-master/themesource/webactions/native/main";
+import * as style6 from "E:/quizapp-mx-course-master/themesource/atlas_core/native/main";
+import * as style7 from "E:/quizapp-mx-course-master/themesource/feedbackmodule/native/main";
+import * as style8 from "E:/quizapp-mx-course-master/themesource/builder/native/main";
+import * as style9 from "E:/quizapp-mx-course-master/themesource/solver/native/main";
+import * as style10 from "E:/quizapp-mx-course-master/themesource/quizapp/native/main";
+import * as style11 from "E:/quizapp-mx-course-master/themesource/extensions/native/main";
+import * as style12 from "E:/quizapp-mx-course-master/themesource/anonymous/native/main";
+import * as style13 from "E:/quizapp-mx-course-master/themesource/datastore/native/main";
+import * as style14 from "E:/quizapp-mx-course-master/themesource/dataservice/native/main";
+import * as style15 from "E:/quizapp-mx-course-master/theme/native/main";
+
+import { flatten } from "mendix/native";
+
+module.exports = flatten([style0, style1, style2, style3, style4, style5, style6, style7, style8, style9, style10, style11, style12, style13, style14, style15]);
